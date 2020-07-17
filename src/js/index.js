9f558a87777630fd8370e1861b6f12f6907d1751
johanuribe03 <johan.uribe@gmail.com>
2020-07-17 00:06:14 +0000

    update 7/16/20 -2

--- src/js/index.js
+++ src/js/index.js
@@ -9,9 +9,16 @@ window.onload = function() {
   console.log("Hello Rigo from the console!");
 };
 
-let generator =>{
+let generator = () => {
+  
     let suite = [ Hearts, Spades, Clubs, Diamonds]
     let value = [1,2,3,4,5,6,7,8,9,10,King,Queen,Jack,Ace]
-}
+
+    let suiteIndex = Math.floor(Math.random() * suite.length);
+    let valueIndex = Math.floor(Math.random() * value.length)
+
+    document.querySelector(".card").classList.add(suite[suiteIndex]);
+    return value[valueIndex];
+};
 
 
